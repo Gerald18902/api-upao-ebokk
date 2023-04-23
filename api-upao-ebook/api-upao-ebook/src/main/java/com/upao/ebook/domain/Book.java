a69e6b5d52adbca0f9f3f9b1f3fe161e31decc21
Gerald18902 <geraldpatrick2014@gmail.com>
2023-04-23 17:10:12 -0500

    Creación de la estructura del proyecto, clases del modelo y configuración del acceso a la bd

---
+++ api-upao-ebook/api-upao-ebook/src/main/java/com/upao/ebook/domain/Book.java
@@ -0,0 +1,33 @@
+package com.upao.ebook.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "books")   //En plural
+@Data   //Getters and setters
+@NoArgsConstructor      //Constructor sin argumentos
+@AllArgsConstructor     //Constructor con todos los argumentos
+@ToString
+public class Book {     //En singular
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name= "title", nullable = false)
+    private String title;
+    @Column(name= "author", nullable = false)
+    private String author;
+    @Column(name= "description", nullable = false)
+    private String description;
+    @Column(name= "image_url", nullable = false)
+    private String imageUrl;
+    @Column(name= "created_at")
+    private LocalDateTime createdAt;
+    @Column(name= "update_at")
+    private LocalDateTime updateAt;
+}
